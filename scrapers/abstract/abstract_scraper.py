52f77323fbd3c705e62b225e0df924911cc36a9d
daniloeder <daniloeder1997@gmail.com>
2024-06-12 13:11:38 -0300

    Use cloudscraper to bypass Cloudflare blocking

--- scrapers/abstract/abstract_scraper.py
+++ scrapers/abstract/abstract_scraper.py
@@ -1,6 +1,6 @@
 import logging
-import requests
-from abc import ABC, abstractmethod
+import cloudscraper
+from abc import ABC
 
 class AbstractScraper(ABC):
     
@@ -9,21 +9,22 @@ class AbstractScraper(ABC):
         self.country = country
         self.logger = logging.getLogger(__name__)
         logging.basicConfig(level=logging.INFO)
-    
+        self.scraper = cloudscraper.create_scraper()
+
     def send_get_request(self, url, **kwargs):
         try:
-            response = requests.get(url, **kwargs)
+            response = self.scraper.get(url, **kwargs)
             response.raise_for_status()
             return response
-        except requests.exceptions.RequestException as e:
+        except cloudscraper.exceptions.RequestException as e:
             self.logger.error(f"GET request failed: {e}")
             return None
 
-    def send_post_request(self, url, **kwargs):
+    def send_post_request(self, url , **kwargs):
         try:
-            response = requests.post(url, **kwargs)
+            response = self.scraper.post(url, **kwargs)
             response.raise_for_status()
             return response
-        except requests.exceptions.RequestException as e:
+        except cloudscraper.exceptions.RequestException as e:
             self.logger.error(f"POST request failed: {e}")
             return None
